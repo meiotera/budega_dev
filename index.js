fe0883c74e6cc6a354eab9a2879c7a1f867b6ea3
Renan Ferreira da Costa <renanfc.re@gmail.com>
2023-09-22 10:39:15 -0300

    Estrutura básica criada no index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Instância do mecânisco de renderização
+const hbs = exphbs.create({
+    // Diretório onde os partials estão localizados
+    partialsDir: ['views/partials']
+});
+
+app.engine('handlebars', hbs.engine);
+// Definindo hanldebars como mecânismo de visualização padrão
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+
+})
+
+
+app.listen(port);
