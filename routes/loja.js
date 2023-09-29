e61441e9b460348f4a1444913b94c97ecde67777
Renan Ferreira da Costa <renanfc.re@gmail.com>
2023-09-29 09:18:45 -0300

    função carregaprodutos retirada, e const fs retirada, pois estou utilizando fora para reutilização de código

--- routes/loja.js
+++ routes/loja.js
@@ -1,33 +1,19 @@
 const express = require('express');
 const router = express.Router()
 const path = require('path');
-const fs = require('fs').promises;
+const carregaProdutos = require('../carrega_produtos')
 const caminho = path.join(__dirname, '../views');
 
-router.get('/loja', (req, res) => {
+router.get('/loja', async (req, res) => {
 
-    async function carregaProdutos() {
-        try {
-            const produtosJSON = await fs.readFile('./json/produtos.json', 'utf8');
-            const dados = JSON.parse(produtosJSON);
-            let produtos = [];
+    try {
+        let produtos = await carregaProdutos();
 
-            dados.forEach(produto => {
-                produtos.push({
-                    nome: produto.nome,
-                    descricao: produto.descricao,
-                    preco: produto.preco
-                });
-            });
+        res.render(`${caminho}/produtos`, { produtos })
 
-            return res.render(`${caminho}/produtos`, { produtos });
-
-        } catch (error) {
-            console.log(error);
-        }
+    } catch (error) {
+        console.log(error)
     }
-    
-    carregaProdutos();
 
 });
 
